bbab34350308d30a994688744c99e81d358d4182
huanxingshi01 <906194591@qq.com>
2020-07-04 10:10:43 +0800

    zuoye

---
+++ Business/Calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def less(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
